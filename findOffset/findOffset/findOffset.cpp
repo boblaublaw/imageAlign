35ae1a7e09eec062a74e8e26eca50e3f8569ad78
Joe Boyle <github@sublimation.org>
2017-01-29 14:33:54 -0800

    sketched out some exception handling on file input

--- findOffset/findOffset/findOffset.cpp
+++ findOffset/findOffset/findOffset.cpp
@@ -1,11 +1,57 @@
 // findOffset.cpp : Defines the entry point for the console application.
 //
 #include "stdafx.h"
+#include "stdlib.h"
+#include <string>
 #include <iostream>
 
-int main()
+class pgmImage
 {
-	std::cout << "Hello World2!" << std::endl;
-	return 0;
+public:
+	char *filename;
+	pgmImage() {};
+	pgmImage(char *f) : filename(f)
+	{
+		std::cout << "reading a pgm file: " << filename << std::endl;
+		if (false) {
+			throw "poop";
+		}
+	}
+	// pgmImage operator + (const CVector&);
+};
+
+void usage(char* progname)
+{
+	std::cout << std::endl;
+	std::cout << progname << " usage:" << std::endl;
+	std::cout << std::endl;
+	std::cout << "     " << progname << " frameA.pgm frameB.pgm" << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+int main(int argc, char* argv[])
+{
+	pgmImage A, B;
+
+	/*
+	check arguments
+	*/
+	if (argc !=3) {
+		usage(argv[0]);
+	}
+	
+	try
+	{
+		A = pgmImage(argv[1]);
+		B = pgmImage(argv[2]);
+	}
+	catch (char *e)
+	{
+		std::cout << "An exception occurred: " << e << std::endl;
+		usage(argv[0]);
+	}
+
+	std::cout << "all good brah" << std::endl;
+	exit(EXIT_SUCCESS);
 }
 
