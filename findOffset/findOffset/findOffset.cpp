b1a714ac7d391acd1a358e083a3b1a60b3e74385
Joe Boyle <github@sublimation.org>
2017-01-29 15:11:59 -0800

    PGM header parsing works, next up is data

--- findOffset/findOffset/findOffset.cpp
+++ findOffset/findOffset/findOffset.cpp
@@ -4,20 +4,125 @@
 #include "stdlib.h"
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <cstring>
+
+const int MAX_TOKENS_PER_LINE = 2000;
+const int MAX_CHARS_PER_LINE = MAX_TOKENS_PER_LINE * 4;
+
+const char* const DELIMITER = " \t";
 
 class pgmImage
 {
 public:
 	char *filename;
+	std::string pgmVersion;
+	int X, Y;
+
 	pgmImage() {};
 	pgmImage(char *f) : filename(f)
 	{
+		std::ifstream fin;
+		char buf[MAX_CHARS_PER_LINE];
+
 		std::cout << "reading a pgm file: " << filename << std::endl;
+		fin.open(filename);
+		if (!fin.good())
+		{
+			throw std::string("Couldn't open the input file: ") + std::string(filename);
+		}
+
+		// first check the file header version
+		fin.getline(buf, MAX_CHARS_PER_LINE);
+		pgmVersion = std::string(buf);
+		if (pgmVersion != "P2")
+		{
+			throw std::string("No idea how to parse a PGM of version ") + pgmVersion;
+		}
+
+		// next parse the resolution
+		{
+			fin.getline(buf, MAX_CHARS_PER_LINE);
+
+			// parse the line into blank-delimited tokens
+			int n = 0; // a for-loop index
+
+			// array to store memory addresses of the tokens in buf
+			const char* token[MAX_TOKENS_PER_LINE] = {}; // initialize to 0
+			// parse the line
+			token[0] = strtok(buf, DELIMITER); // first token
+			if (token[0]) // zero if line is blank
+			{
+				for (n = 1; n < MAX_TOKENS_PER_LINE; n++)
+				{
+					token[n] = strtok(0, DELIMITER); // subsequent tokens
+					if (!token[n]) break; // no more tokens
+				}
+			}
+			if (n != 2)
+			{
+				throw std::string("there are more than 2 tokens on the resolution line of the header!");
+			}
+			try
+			{
+				X = std::stoi(std::string(token[0]));
+			}
+			catch (...)
+			{
+				throw std::string("Failed to parse X resolution to integers: ") + std::string(token[0]);
+			}
+			try
+			{
+				Y = std::stoi(std::string(token[1]));
+			}
+			catch (...)
+			{
+				throw std::string("Failed to parse Y resolution to integers: ") + std::string(token[1]);
+			}
+		}
+
+		// first check the file header version
+		fin.getline(buf, MAX_CHARS_PER_LINE);
+		if (std::string(buf) != "255")
+		{
+			throw std::string("No idea how to parse a PGM maxValue of ") + std::string(buf);
+		}
+
+		// read each line of the file
+		while (!fin.eof())
+		{
+			// read an entire line into memory
+			fin.getline(buf, MAX_CHARS_PER_LINE);
+
+			// parse the line into blank-delimited tokens
+			int n = 0; // a for-loop index
+
+			// array to store memory addresses of the tokens in buf
+			const char* token[MAX_TOKENS_PER_LINE] = {}; // initialize to 0
+
+			// parse the line
+			token[0] = strtok(buf, DELIMITER); // first token
+			if (token[0]) // zero if line is blank
+			{
+				for (n = 1; n < MAX_TOKENS_PER_LINE; n++)
+				{
+					token[n] = strtok(0, DELIMITER); // subsequent tokens
+					if (!token[n]) break; // no more tokens
+				}
+			}
+
+			// process (print) the tokens
+			for (int i = 0; i < n; i++) // n = #of tokens
+				std::cout << "Token[" << i << "] = " << token[i] << std::endl;
+			std::cout << std::endl;
+		}
+
+
 		if (false) {
 			throw "poop";
 		}
 	}
-	// pgmImage operator + (const CVector&);
+	// pgmImage operator + (const pgmImage&);
 };
 
 void usage(char* progname)
@@ -44,14 +149,19 @@ int main(int argc, char* argv[])
 	{
 		A = pgmImage(argv[1]);
 		B = pgmImage(argv[2]);
+
+
 	}
 	catch (char *e)
 	{
 		std::cout << "An exception occurred: " << e << std::endl;
 		usage(argv[0]);
 	}
-
-	std::cout << "all good brah" << std::endl;
+	catch (std::string e)
+	{
+		std::cout << "An exception occurred: " << e.c_str() << std::endl;
+		usage(argv[0]);
+	}
 	exit(EXIT_SUCCESS);
 }
 
